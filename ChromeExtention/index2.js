47f626c24b06dbd73f0da63ba70e94cc26909e9d
HassanBaji <120788169+HassanBaji@users.noreply.github.com>
2023-03-22 11:16:32 +0300

    12

--- ChromeExtention/index2.js
+++ ChromeExtention/index2.js
@@ -5,10 +5,19 @@ const ulEl  = document.getElementById("ul-el")
 
 saveBtn.addEventListener("click", function(){
 myLeads.push(inputEl.value)
-ulEl.textContent = ""
-for (let i = 0; i < myLeads.length; i++){
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li> "
-}
+inputEl.value=  ""
+renderLeads()
+
 })
 
+function renderLeads(){
+    let myList =  "";
+for (let i = 0; i < myLeads.length; i++){
+    myList += 
+            `<li id='li-el'>
+            <a id = 'a-el' target = '_blank' href = '${myLeads[i]}'>${myLeads[i]} </a>
+            </li>`
+}
+ulEl.innerHTML =  myList
+}
 
